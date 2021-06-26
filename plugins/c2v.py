2710b9788b70accf80d3a6f3b1887623ee98b6fd
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-26 14:47:14 +0530

    Add files via upload

---
+++ plugins/c2v.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ©️ @AmineSoukara
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import time
+import random
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from xtranslation import XTranslation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+from pyrogram import Client, Filters
+from helper_funcs.help_Nekmo_ffmpeg import take_screen_shot
+from helper_funcs.chat_base import TRChatBase
+from helper_funcs.display_progress import progress_for_pyrogram
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+# https://stackoverflow.com/a/37631799/4723940
+from PIL import Image
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["renamevideo", "v"]))
+async def rename_video(bot, update):
+    if update.from_user.id in Config.BANNED_USERS:
+        await update.reply_text("You are B A N N E D 🤣🤣🤣🤣")
+        return
+    TRChatBase(update.from_user.id, update.text, "rename")
+    update_channel = Config.UPDATE_CHANNEL
+    if update_channel:
+        try:
+            user = await bot.get_chat_member(update_channel, update.chat.id)
+            if user.status == "kicked":
+               await update.reply_text("🤭 Sorry Dude, You are **B A N N E D 🤣🤣🤣**")
+               return
+        except UserNotParticipant:
+            #await update.reply_text(f"Join @{update_channel} To Use Me")
+            await update.reply_text(
+                text="[●](https://i.imgur.com/t1JsZ0I.gif) **Join My Updates Channel To Mse Me : **",
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="Subscribe", url=f"https://t.me/{update_channel}")]
+              ])
+            )
+            return
+        except Exception:
+            await update.reply_text("Something Wrong. Contact @AmineSoukara")
+            return
+    if (" " in update.text) and (update.reply_to_message is not None):
+        cmd, file_name = update.text.split(" ", 1)
+        if len(file_name) > 64:
+            await update.reply_text(
+                XTranslation.IFLONG_FILE_NAME.format(
+                    alimit="64",
+                    num=len(file_name)
+                )
+            )
+            return
+        description = XTranslation.CUSTOM_CAPTION_UL_FILE
+        download_location = Config.DOWNLOAD_LOCATION + "/"
+        b = await bot.send_message(
+            chat_id=update.chat.id,
+            text=XTranslation.DOWNLOAD_START,
+            reply_to_message_id=update.message_id
+        )
+        c_time = time.time()
+        the_real_download_location = await bot.download_media(
+            message=update.reply_to_message,
+            file_name=download_location,
+            progress=progress_for_pyrogram,
+            progress_args=(
+                XTranslation.DOWNLOAD_START,
+                b,
+                c_time
+            )
+        )
+        if the_real_download_location is not None:
+            try:
+                await bot.edit_message_text(
+                    text=XTranslation.SAVED_RECVD_DOC_FILE,
+                    chat_id=update.chat.id,
+                    message_id=b.message_id
+                )
+            except:
+                pass
+            new_file_name = download_location + file_name
+            os.rename(the_real_download_location, new_file_name)
+            await bot.edit_message_text(
+                text=XTranslation.UPLOAD_START_VIDEO,
+                chat_id=update.chat.id,
+                message_id=b.message_id
+                )
+            logger.info(the_real_download_location)
+            width = 0
+            height = 0
+            duration = 0
+            metadata = extractMetadata(createParser(new_file_name))
+            try:
+             if metadata.has("duration"):
+                duration = metadata.get('duration').seconds
+            except:
+              pass
+            thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
+            if not os.path.exists(thumb_image_path):
+               try:
+                    thumb_image_path = await take_screen_shot(new_file_name, os.path.dirname(new_file_name), random.randint(0, duration - 1))
+               except:
+                    thumb_image_path = None
+            else:
+                width = 0
+                height = 0
+                metadata = extractMetadata(createParser(thumb_image_path))
+                if metadata.has("width"):
+                    width = metadata.get("width")
+                if metadata.has("height"):
+                    height = metadata.get("height")
+                # resize image
+                # ref: https://t.me/PyrogramChat/44663
+                # https://stackoverflow.com/a/21669827/4723940
+                Image.open(thumb_image_path).convert("RGB").save(thumb_image_path)
+                img = Image.open(thumb_image_path)
+                # https://stackoverflow.com/a/37631799/4723940
+                # img.thumbnail((90, 90))
+                img.resize((320, height))
+                img.save(thumb_image_path, "JPEG")
+                # https://pillow.readthedocs.io/en/3.1.x/reference/Image.html#create-thumbnails
+            c_time = time.time()
+            await bot.send_video(
+                chat_id=update.chat.id,
+                video=new_file_name,
+                duration=duration,
+                width=width,
+                height=height,
+                supports_streaming=True,
+                thumb=thumb_image_path,
+                caption=file_name,
+                # reply_markup=reply_markup,
+                reply_to_message_id=update.reply_to_message.message_id,
+                progress=progress_for_pyrogram,
+                progress_args=(
+                    XTranslation.UPLOAD_START_VIDEO,
+                    b, 
+                    c_time
+                )
+            )
+            try:
+                os.remove(new_file_name)
+                #os.remove(thumb_image_path)
+            except:
+                pass
+            await bot.edit_message_text(
+                text=XTranslation.AFTER_SUCCESSFUL_UPLOAD_MSG,
+                chat_id=update.chat.id,
+                message_id=b.message_id,
+                disable_web_page_preview=True
+            )
+    else:
+        await bot.send_message(
+            chat_id=update.chat.id,
+            text=XTranslation.REPLY_TO_DOC_FOR_RENAME_VIDEO,
+            reply_to_message_id=update.message_id
+        )
---
+++ plugins/commands.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ©️ @AmineSoukara
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+from pyrogram import ReplyKeyboardMarkup, InlineKeyboardMarkup, InlineKeyboardButton
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(853393439)
+    return expires_at
+
+#@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+#async def start(bot, update):
+    # logger.info(update)
+    #TRChatBase(update.from_user.id, update.text, "/start")
+    #await bot.send_message(
+                #chat_id=update.chat.id,
+                #text=Translation.START_MSG.format(update.from_user.first_name),
+                #reply_markup = InlineKeyboardMarkup(
+                    #[[InlineKeyboardButton("🗣 Support Group", url=f"t.me/damienhelp"),
+                    #InlineKeyboardButton("💬 Updates Channel", url="t.me/DamienSoukara")],
+                    #[InlineKeyboardButton("⭕ Developer ⭕", url="t.me/AmineSoukara")
+                    #]]
+                #)
+            #)
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await update.reply_text(
+                text=Translation.HELP_USER.format(update.from_user.first_name),
+                reply_markup=InlineKeyboardMarkup([
+                    [ InlineKeyboardButton(text="📢 More Help", url=f"https://t.me/DamienHelp")]
+              ])
+            )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["me"]))
+async def get_me_info(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/me")
+    chat_id = str(update.from_user.id)
+    chat_id, plan_type, expires_at = GetExpiryDate(chat_id)
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.CURENT_PLAN_DETAILS.format(chat_id, plan_type, expires_at),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
+async def donate(bot, update):
+       await bot.send_message(
+             chat_id=update.chat.id,
+             text="I am very happy to listen you this word, making of this bot take lot of work and time so please donate by pressing this button present below",
+             reply_markup=InlineKeyboardMarkup(
+             [
+               [
+                 InlineKeyboardButton('Donate 💰', url='http://paypal.me/AmineSoukara')
+               ]
+             ]
+           )
+          )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["about"]))
+async def about(bot, update):
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.ABOUT.format(update.from_user.first_name),
+        parse_mode="markdown",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(c, m):
+      button = [[
+                InlineKeyboardButton("💬 Updates Channel", url="t.me/DamienSoukara"),
+                InlineKeyboardButton("🗣 Support Group", url="t.me/damienhelp"),
+                ],
+                [
+                InlineKeyboardButton("ℹ About", callback_data="about"),
+                InlineKeyboardButton("🤔 Help", callback_data="morehelp")
+                ],
+                [InlineKeyboardButton("🤴 Developer 🤴", url="t.me/AmineSoukara")]]
+      markup = InlineKeyboardMarkup(button)
+      await c.send_message(chat_id=m.chat.id,
+                           text=Translation.START_MSG.format(m.from_user.first_name),
+                           reply_to_message_id=m.message_id,
+                           reply_markup=markup)
+
