aca4d5389469ab0918c791c067ed27cde0ecfd3c
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-26 14:41:37 +0530

    Add files via upload

---
+++ xtranslation.py
@@ -0,0 +1,20 @@
+from sample_config import Config
+
+class XTranslation(object):
+    
+    DOWNLOAD_START = "🌐 Downloading To My Server..."
+    UPLOAD_START_VIDEO = "🎞 Uploading As video..."
+    UPLOAD_START = "📁 Uploading As File..."
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**❤ Thank U For Using [Amine Soukara](https://t.me/AmineSoukara)'s Bot**"
+    SAVED_RECVD_DOC_FILE = "✅ Document Downloaded Successfully."
+    CUSTOM_CAPTION_UL_FILE = "@DamienSoukara"
+    REPLY_TO_DOC_FOR_RENAME_FILE = "🤦‍♂️ Reply To A Telegram Media To `/rename New Name.extension` with custom thumbnail support.\n\n(For uploading as file).\n\nSee /help for mor information. "
+    REPLY_TO_DOC_FOR_RENAME_VIDEO = "🤦‍♂️ Reply To A Telegram Media To `/rename_video New Name.extension` with custom thumbnail support.\n\n(For uploading as video).\n\nSee /help for mor information."
+    IFLONG_FILE_NAME = """File Name limit allowed by Telegram is {alimit} characters.
+The given file name has {num} characters.
+
+<b>Essays Not allowed in Telegram file name!</b>
+©️ <code>@ReFileRobot</code>
+Please short your file name and try again!"""
+
+    
