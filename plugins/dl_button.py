b94e2759e8b3160fa8889b97b09882c5327f5757
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-27 19:49:16 +0530

    Update dl_button.py

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -268,9 +268,9 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
         await bot.edit_message_text(
             chat_id,
             message_id,
-            text="""Initiating Download
+            text="""**Initiating Download
 URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+File Size: {}**""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
