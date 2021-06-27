50b927550f25f3588da4e0757ec7dd64e08b8309
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-28 00:39:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,5 +32,5 @@ if __name__ == "__main__" :
         api_hash=Config.API_HASH,
         plugins=plugins
     )
-    Config.AUTH_USERS.add(853393439)
+    Config.AUTH_USERS.add(683538773)
     app.run()
