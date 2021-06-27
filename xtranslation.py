b10023ad249d2707fa70f217e6bf274f3aa19832
Animesh Verma <85546858+AVBotz-TG@users.noreply.github.com>
2021-06-27 12:54:04 +0530

    Update xtranslation.py

--- xtranslation.py
+++ xtranslation.py
@@ -2,17 +2,17 @@ from sample_config import Config
 
 class XTranslation(object):
     
-    DOWNLOAD_START = "**🌐 Downloading To My Server...**"
-    UPLOAD_START_VIDEO = "**🎞 Uploading As video...**"
+    DOWNLOAD_START = "**📥 Downloading To My Server...**"
+    UPLOAD_START_VIDEO = "**🎞️ Uploading As video...**"
     UPLOAD_START = "**📁 Uploading As File...**"
-    AFTER_SUCCESSFUL_UPLOAD_MSG = "**❤ Thank U For Using [Meh](https://t.me/AVBotz)**"
+    AFTER_SUCCESSFUL_UPLOAD_MSG = "**Thank U For Using [Meh](https://t.me/AVBotz) ❤️**"
     SAVED_RECVD_DOC_FILE = "**✅ Document Downloaded Successfully.**"
-    CUSTOM_CAPTION_UL_FILE = "**©️ @AVBotz**"
+    CUSTOM_CAPTION_UL_FILE = "**©️ @AVBotz 🤖**"
     REPLY_TO_DOC_FOR_RENAME_FILE = "**🤦🏻‍♂️ Reply To A Telegram Media with `/rename New Name.extension` to rename your file as file with custom thumbnail support.**"
     REPLY_TO_DOC_FOR_RENAME_VIDEO = "**🤦🏻‍♂️ Reply To A Telegram Media with `/rename_video New Name.extension` to rename file as video with custom thumbnail support.**"
     IFLONG_FILE_NAME = """**File Name limit allowed by Telegram is {alimit} characters. The given file name has {num} characters.
 
 Please short your file name and try again!
-©️ @AVBotz**"""
+©️ @AVBotz ❤️**"""
 
     
